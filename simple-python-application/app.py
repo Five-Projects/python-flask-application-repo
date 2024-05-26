08fdfde82ab1b66d754f6dc5dc6cad04f73fb16b
Manoj Indalkar <73096275+mnidevops@users.noreply.github.com>
2024-05-26 14:18:47 +0530

    Update app.py

--- simple-python-application/app.py
+++ simple-python-application/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World! This is an AWS CICD Pipeline to deploy a Python Flask application.'
+    return 'Hello, everyone! This is an AWS CICD Pipeline to deploy a Python Flask application.'
 
 if __name__ == '__main__':
     app.run()
