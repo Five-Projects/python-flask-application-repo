066558bc6cbc0ae3ce90956fb96cf44820fe38a9
Manoj Indalkar <73096275+mnidevops@users.noreply.github.com>
2024-05-26 13:53:07 +0530

    Update app.py

--- simple-python-application/app.py
+++ simple-python-application/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! This is a simple Python Flask application.'
+    return 'Hello, everyone! This is a simple Python Flask application.'
 
 if __name__ == '__main__':
     app.run()
